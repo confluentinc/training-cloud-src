a8be12af5964ca3d0dc6de5ee945712a49b52fb7
Borja Hernandez Crespo <bcrespo@confluent.io>
2022-04-14 17:13:06 +0200

    Update install-completion.sh

--- install-completion.sh
+++ install-completion.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-# Install Bash completion 
-apt-get install bash-completion
-
 # Set the confluent completion code for Bash to a file that’s sourced on login
-confluent completion bash > /etc/bash_completion.d/confluent
+/bin/confluent completion bash > /etc/bash_completion.d/confluent
 
 # Load the confluent completion code for Bash into the current shell 
 source /etc/bash_completion.d/confluent
 
 # Add the source command to ~/.bashrc to enable completions for new terminals
 echo "source /etc/bash_completion.d/confluent" >> /home/training/.bashrc
+
