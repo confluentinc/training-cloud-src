f7f6c38df7a3e4cabb823144513800dc87f82e1a
Borja Hernandez Crespo <bcrespo@confluent.io>
2022-04-14 17:28:37 +0200

    Update update-completion.sh

--- update-completion.sh
+++ update-completion.sh
@@ -3,9 +3,6 @@
 # Set the confluent completion code for Bash to a file that’s sourced on login
 /bin/confluent completion bash > /etc/bash_completion.d/confluent
 
-# Load the confluent completion code for Bash into the current shell 
-source /etc/bash_completion.d/confluent
-
 # Add the source command to ~/.bashrc to enable completions for new terminals
 echo "source /etc/bash_completion.d/confluent" >> /home/training/.bashrc
 
