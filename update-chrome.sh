5cbb30f9ca3aa7e73b0d265cf05edeca872f7534
Borja Hernandez Crespo <bcrespo@confluent.io>
2022-04-14 14:46:13 +0200

    Create update-chrome.sh

---
+++ update-chrome.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Updating Google Chrome..."
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo apt install ./google-chrome-stable_current_amd64.deb
